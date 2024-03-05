41c879604f438ed24d1ba6490f271038aa476b5e
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 02:15:30 +0300

    Add files via upload

---
+++ 2.unite uygulama 24/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int toplam = 0;
+            for (int i = sayi1; i < sayi2; i++)
+            {
+                toplam+= i;
+               
+            }
+            MessageBox.Show("toplam degeriniz" + toplam.ToString());
+        }
+    }
+}
